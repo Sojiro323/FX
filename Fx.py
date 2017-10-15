48f10931e190591bfbc7d390e5bcf7a0846319a7
Sojiro323 <m23622059@gmail.com>
2017-10-16 02:12:59 +0900

    add get_candles

--- Fx.py
+++ Fx.py
@@ -5,17 +5,20 @@ import sys
 
 
 
-domainDict = { 'live' : 'stream-fxtrade.oanda.com',
-               'demo' : 'stream-fxpractice.oanda.com' }
-
-
 
 class Myoanda:
   def __init__(self):
     self.access_token = '70b5158af805e0f3f427d9c2c8864cf0-a9bda9dcfa16842c09e39db487950ed7'
     self.account_id = '3205831'
-    self.environment = 'demo'
-    self.domain = domainDict[self.environment]
+    self.domainDict = { 'stream_live' : 'stream-fxtrade.oanda.com',
+                        'stream_demo' : 'stream-fxpractice.oanda.com',
+                        'live' : 'api-fxpractice.oanda.com',
+                        'demo' : 'api-fxtrade.oanda.com'}
+    self.domain = self.domainDict['demo']
+    self.instruments = 'USD_JPY'
+    self.headers = {'Authorization' : 'Bearer ' + self.access_token,
+                    # 'X-Accept-Datetime-Format' : 'unix'
+                    }
 
     usage = "usage: %prog [options]"
     parser = OptionParser(usage)
@@ -30,24 +33,7 @@ class Myoanda:
       self.displayHeartbeat = True
 
 
-  def get_prices(self, instruments):
-
-    try:
-
-       s = requests.Session()
-       url = "https://" + self.domain + "/v1/prices"
-       headers = {'Authorization' : 'Bearer ' + self.access_token,
-                   # 'X-Accept-Datetime-Format' : 'unix'
-                   }
-       params = {'instruments' : instruments, 'accountId' : self.account_id}
-       req = requests.Request('GET', url, headers = headers, params = params)
-       pre = req.prepare()
-       response = s.send(pre, stream = True, verify = True)
-
-    except Exception as e:
-      s.close()
-      print("Caught exception when connecting to stream\n" + str(e))
-      sys.exit()
+  def stream(self, response):
 
     if response.status_code != 200:
         print(response.text)
@@ -62,3 +48,43 @@ class Myoanda:
 
             if "instrument" in msg or "tick" in msg or self.displayHeartbeat:
                 print(line)
+
+
+  def get_prices(self,**params):
+    
+    self.domain = self.domainDict['stream_demo']
+    params['account_Id'] = self.account_id
+    if 'instruments' not in params: params['instruments'] = self.instruments
+
+    try:
+
+      s = requests.Session()
+      url = "https://" + self.domain + "/v1/prices"
+      req = requests.Request('GET', url, headers = self.headers, params = params)
+      pre = req.prepare()
+      response = s.send(pre, stream = True, verify = True)
+      self.stream(response)
+
+    except Exception as e:
+      s.close()
+      print("Caught exception when connecting to stream\n" + str(e))
+      sys.exit()
+
+  
+  def get_candles(self,**params):
+
+    self.domain = self.domainDict['demo']
+    params['accountId'] = self.account_id
+    if 'instrument' not in params: params['instrument'] = self.instruments
+    if 'alignmentTimeZone' not in params: params['alignmentTimeZone'] = "Asia/Tokyo"
+
+    try:
+
+      url = "https://" + self.domain + "/v1/candles"
+
+      r = requests.get(url, headers=self.headers, params=params)
+      print(params)
+      print(r.text)
+    except Exception as e:
+      print("Caught exception when connecting to stream\n" + str(e))
+      sys.exit()
---
+++ main.py
@@ -0,0 +1,31 @@
+import Fx
+
+'''
+class Myoanda
+def get_prices():
+    instruments(deault:"USD_JPY")
+    
+def get_candles():
+    instrument(deault:"USD_JPY")
+    granularity(default:"S5"): Optional The time range represented by each candlestick. The value specified will determine the alignment of the first candlestick
+        “S5” - 5 seconds
+        “M1” - 1 minute
+        “H1” - 1 hour
+        “D” - 1 Day
+        “W” - 1 Week
+        “M” - 1 Month
+    count(default:500): Optional The number of candles to return in the response.The maximum acceptable value for count is 5000.
+    start(default:Null): Optional The start timestamp for the range of candles requested.
+    end(default:now): Optional The end timestamp for the range of candles requested.
+    candleFormat(default:“bidask”):“midpoint” - Midpoint based candlesticks. “bidask” - Bid/Ask based candlesticks.
+    includeFirst(default:true):If it is set to “true”, the candlestick covered by the start timestamp will be returned. not be returned.
+    dailyAlignment: Optional The hour of day used to align candles with hourly, daily, weekly, or monthly granularity.
+                    The value specified is interpretted as an hour   in the timezone set through the alignmentTimezone parameter and must be an integer between 0 and 23.
+    alignmentTimezone(default:Asia/Tokyo):
+
+'''
+
+oanda = Fx.Myoanda()
+
+#oanda.get_prices()
+oanda.get_candles()
