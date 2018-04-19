aed16032066bd90332b7aee6de5cbfc4ba72fa4b
Sojiro323 <m23622059@gmail.com>
2018-04-19 18:17:25 +0900

    add api func

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import pandas as pd
 import requests
 import json
@@ -5,33 +6,20 @@ from optparse import OptionParser
 import sys
 
 
-
-
 class oanda:
-  def __init__(self, access_token, account_id):
-    self.access_token = access_token
-    self.account_id = account_id
-    self.domainDict = { 'stream_live' : 'stream-fxtrade.oanda.com',
+
+  def __init__(self, accessToken, accountId, domain_type, instrument):
+    self.accessToken = accessToken
+    self.accountId = accountId
+    self.domainDict = { 'stream_live' : 'stream-fxtrade.oanda.com', #production
                         'stream_demo' : 'stream-fxpractice.oanda.com',
-                        'live' : 'api-fxpractice.oanda.com',
-                        'demo' : 'api-fxtrade.oanda.com'}
-    self.domain = self.domainDict['demo']
-    self.instruments = 'USD_JPY'
-    self.headers = {'Authorization' : 'Bearer ' + self.access_token,
-                    # 'X-Accept-Datetime-Format' : 'unix'
-                    }
-
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("-b", "--displayHeartbeat", dest = "verbose", action = "store_true",
-                      help = "Display HeartBeat in streaming data")
-    self.displayHeartbeat = False
-
-    (options, args) = parser.parse_args()
-    if len(args) > 1:
-      parser.error("incorrect number of arguments")
-    if options.verbose:
-      self.displayHeartbeat = True
+                        'live' : 'api-fxtrade.oanda.com', #production
+                        'demo' : 'api-fxpractice.oanda.com'}
+    self.domain_type = domain_type
+    self.domain = self.domainDict[self.domain_type]
+    self.domain_stream = self.domainDict["stream_" + self.domain_type]
+    self.instrument = instrument
+    self.headers = {'Authorization' : 'Bearer ' + self.accessToken}
 
 
   def stream(self, response):
@@ -47,20 +35,32 @@ class oanda:
                 print("Caught exception when converting message into json\n" + str(e))
                 sys.exit()
 
-            if "instrument" in msg or "tick" in msg or self.displayHeartbeat:
+            if "instrument" in msg or "tick" in msg:
                 print(line)
 
+  def connect(self, type, url, params, headers):
+    url = "https://" + self.domain + url
+    try:
+      if type == 'GET': r = requests.get(url, headers=headers, params=params)
+      elif type == 'POST': r = requests.post(url, headers=headers, data=params)
+      elif type == 'DELETE': r = requests.delete(url, headers=headers)
+      elif type == 'PATCH': r = requests.patch(url, params headers=headers)
+      return r
+    except Exception as e:
+      print("Error : " + str(e))
+      return None
 
+
+#ストリームで現在の値段を取得
   def get_prices(self,**params):
 
-    self.domain = self.domainDict['stream_demo']
-    params['account_Id'] = self.account_id
-    if 'instruments' not in params: params['instruments'] = self.instruments
+    params['accountId'] = self.accountId
+    params['instruments'] = self.instrument
 
     try:
 
       s = requests.Session()
-      url = "https://" + self.domain + "/v1/prices"
+      url = "https://" + self.domain_stream + "/v1/prices"
       req = requests.Request('GET', url, headers = self.headers, params = params)
       pre = req.prepare()
       response = s.send(pre, stream = True, verify = True)
@@ -71,33 +71,83 @@ class oanda:
       print("Caught exception when connecting to stream\n" + str(e))
       sys.exit()
 
-
+#過去のローソクを取得
   def get_candles(self,**params):
 
-    self.domain = self.domainDict['demo']
-    params['accountId'] = self.account_id
-    if 'instrument' not in params: params['instrument'] = self.instruments
+    params['accountId'] = self.accountId
+    params['instrument'] = self.instrument
     if 'alignmentTimeZone' not in params: params['alignmentTimeZone'] = "Asia/Tokyo"
+    url = "/v1/candles"
 
-    try:
-
-      url = "https://" + self.domain + "/v1/candles"
-
-      r = requests.get(url, headers=self.headers, params=params)
-      print(params)
-      print(r.text)
-      return r
-    except Exception as e:
-      print("Caught exception when connecting to stream\n" + str(e))
-      return None
+    return self.connect("GET", url, params, self.headers)
 
 
-  def get_BollingerBand(self, **params):
+"""----------------------ticket----------------------"""
+#未決済チケットの取得
+  def get_tickets(self, **params):
 
-    candles = self.get_candles(params)
-    base = pd.Series.rolling(candles, window=25).mean()
-    sigma = pd.Series.rolling(candles, window=25).std(ddof=0)
-    upper_sigma = base + sigma
-    upper2_sigma = base + sigma * 2
-    low_sigma = base - sigma
-    low2_sigma = base = sigma * 2
+    params["instrument"] = self.instrument
+    url = "/v1/accounts/" + self.accountId + "/trades"
+    return self.connect("GET", url, params, self.headers)
+
+#チケットの変更
+  def get_tickets(self, trade_id, **params):
+
+    url = "/v1/accounts/" + self.accountId + "/trades" + trade_id
+    return self.connect("PATCH", url, params, self.headers)
+
+#チケットクローズ
+  def close_tickets(self, trade_id):
+
+    url = "/v1/accounts/" + self.accountId + "/trades/" + trade_id
+    return self.connect("DELETE", url, {}, self.headers)
+
+
+
+
+"""----------------------position----------------------"""
+#未決済ポジションの取得
+  def get_positions(self):
+
+    url = "/v1/accounts/" + self.accountId + "/positions/" + self.instrument
+    return self.connect("GET", url, {}, self.headers)
+
+#ポジションクローズ
+  def close_positions(self):
+
+    url = "/v1/accounts/" + self.accountId + "/positions/" + self.instrument
+    return self.connect("DELETE", url, {}, self.headers)
+
+
+
+"""----------------------order----------------------"""
+#注文
+  def order(self,**params):
+
+    params['accountId'] = self.accountId
+    params['instrument'] = self.instrument
+    url = "/v1/accounts/" + self.accountId + "/orders"
+    headers = self.headers
+    headers['X-Accept-Datetime-Format'] = 'unix'
+
+
+    return self.connect("POST", url, params, headers)
+
+#未決済注文の取得
+  def get_orders(self, **params):
+
+    params["instrument"] = self.instrument
+    url = "/v1/accounts/" + self.accountId + "/orders"
+    return self.connect("GET", url, params, self.headers)
+
+#注文の変更
+  def get_orders(self, order_id, **params):
+
+    url = "/v1/accounts/" + self.accountId + "/orders/" + order_id
+    return self.connect("PATCH", url, params, self.headers)
+
+#注文クローズ
+  def close_orders(self, order_id):
+
+    url = "/v1/accounts/" + self.accountId + "/trades/" + trade_id
+    return self.connect("DELETE", url, {}, self.headers)
